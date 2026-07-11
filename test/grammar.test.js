1f708411580f068c90f4b6806a35c4dc83cdb762
Conrad Buck <conartist6@gmail.com>
2026-07-10 19:41:11 -0600

    porcelain printing work

--- test/grammar.test.js
+++ test/grammar.test.js
@@ -2,7 +2,7 @@ import { buildTag } from 'bablr';
 import { dedent } from '@qnighy/dedent';
 import language from '@bablr/language-en-cstml-json';
 import { expect } from 'expect';
-import { printPrettyCSTML } from '@bablr/helpers/tree';
+import { printCSTML } from '@bablr/helpers/tree';
 import { m } from '@bablr/helpers/grammar';
 
 const buildJSONTag = (matcher) => {
@@ -10,7 +10,7 @@ const buildJSONTag = (matcher) => {
 };
 
 const print = (tree) => {
-  return printPrettyCSTML(tree);
+  return printCSTML(tree);
 };
 
 describe('@bablr/language-en-cstml-json', () => {
